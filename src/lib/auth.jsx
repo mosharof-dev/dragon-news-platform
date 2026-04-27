7f550e8eeead7c2a1799d1083f6e72ce99c730f2
mosharof-dev <mosharof.dev@gmail.com>
2026-04-28 00:45:31 +0600

    feat: integrate better-auth for authentication; add MongoDB adapter and create auth client

---
+++ src/lib/auth.jsx
@@ -0,0 +1,20 @@
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+
+
+
+const client = new MongoClient(process.env.MONGODB_URL);
+const db = client.db();
+
+
+export const auth = betterAuth({
+emailAndPassword: { 
+    enabled: true, 
+  }, 
+  
+   database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
